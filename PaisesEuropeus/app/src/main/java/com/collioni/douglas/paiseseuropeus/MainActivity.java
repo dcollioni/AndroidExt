e7b1c0ce960da5cdbd9568692beb932cabe2ba8d
Douglas Collioni <dcollioni@gmail.com>
2015-03-21 08:16:52 -0300

    Projeto PaisesEuropeus completo

--- PaisesEuropeus/app/src/main/java/com/collioni/douglas/paiseseuropeus/MainActivity.java
+++ PaisesEuropeus/app/src/main/java/com/collioni/douglas/paiseseuropeus/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -12,8 +13,7 @@ import android.widget.ListView;
 
 public class MainActivity extends ActionBarActivity {
 
-    // TODO: declarar ListView
-
+    ListView lvPaises;
     Button btNenhuma, btUnica1, btUnica2, btMultipla1, btMultipla2;
 
     @Override
@@ -21,8 +21,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO: pegar ListView pelo ID
-
+        lvPaises = (ListView) findViewById(R.id.lvPaises);
         btNenhuma = (Button) findViewById(R.id.btNenhuma);
         btUnica1 = (Button) findViewById(R.id.btUnica1);
         btUnica2 = (Button) findViewById(R.id.btUnica2);
@@ -32,35 +31,79 @@ public class MainActivity extends ActionBarActivity {
         btNenhuma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: configurar lista
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter
+                                                    .createFromResource(
+                                                            getBaseContext(),
+                                                            R.array.paises,
+                                                            android.R.layout.simple_list_item_1
+                                                    );
+
+                lvPaises.setChoiceMode(ListView.CHOICE_MODE_NONE);
+                lvPaises.setAdapter(adapter);
             }
         });
 
         btUnica1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: configurar lista
+                ArrayAdapter<CharSequence> adapter =
+                                    ArrayAdapter
+                                         .createFromResource(
+                                                 getBaseContext(),
+                                                 R.array.paises,
+                                                 android.R.layout.simple_list_item_single_choice
+                                         );
+
+                lvPaises.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+                lvPaises.setAdapter(adapter);
             }
         });
 
         btUnica2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: configurar lista
+                ArrayAdapter<CharSequence> adapter =
+                                ArrayAdapter
+                                .createFromResource(
+                                    getBaseContext(),
+                                    R.array.paises,
+                                    android.R.layout.simple_list_item_activated_1
+                                );
+
+                lvPaises.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+                lvPaises.setAdapter(adapter);
             }
         });
 
         btMultipla1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: configurar lista
+                ArrayAdapter<CharSequence> adapter =
+                                ArrayAdapter
+                                .createFromResource(
+                                    getBaseContext(),
+                                    R.array.paises,
+                                    android.R.layout.simple_list_item_multiple_choice
+                                );
+
+                lvPaises.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                lvPaises.setAdapter(adapter);
             }
         });
 
         btMultipla2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: configurar lista
+                ArrayAdapter<CharSequence> adapter =
+                                ArrayAdapter
+                                .createFromResource(
+                                    getBaseContext(),
+                                    R.array.paises,
+                                    android.R.layout.simple_list_item_checked
+                                );
+
+                lvPaises.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                lvPaises.setAdapter(adapter);
             }
         });
     }
